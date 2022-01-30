6d5e1cb5f1a6244f7539f0017a7cb4a8201e2159
MiniPeWZ <alexander.arum@outlook.com>
2022-01-30 14:46:25 +0100

    databasefaen

--- python-db.py
+++ python-db.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb =mysql.connector.connect(host="localhost", user="root", passwd="Muffe123")
+mydb =mysql.connector.connect(host="localhost", user="root", passwd="")
 
 mycursor =mydb.cursor()
 
